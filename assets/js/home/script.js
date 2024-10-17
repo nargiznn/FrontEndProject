5454d193dff2e99db6ba915c2d76aaac720042f4
Nergiz Nadirli <nergiznadirli@Nergiz-Air.bbrouter>
2024-10-17 22:31:56 +0400

    home area

---
+++ assets/js/home/script.js
@@ -0,0 +1,58 @@
+let searchButtons = document.getElementsByClassName("Searchmodal");
+let modal__items = document.getElementsByClassName("SearchModal__main");
+let modal = false;
+
+for (let i = 0; i < searchButtons.length; i++) {
+    searchButtons[i].addEventListener('click', searchModal);
+}
+
+function searchModal() {
+    modal = !modal;
+    console.log(modal);
+
+    // Tüm modal__items üzerinde döngü kurarak 'open' class'ını ekleyip çıkar
+    for (let i = 0; i < modal__items.length; i++) {
+        modal__items[i].classList.toggle('open');
+    }
+}
+
+
+
+// "use script";
+// let rightIcon=document.querySelector("#slider-area .slider .icons .right");
+// let leftIcon=document.querySelector("#slider-area .slider .icons .left");
+// function rightSlider() {
+//     let activeImage=document.querySelector(".active-img");
+//     if(activeImage.nextElementSibling!=null){
+//         activeImage.classList.remove("active-img"); 
+//         activeImage.nextElementSibling.classList.add("active-img")
+//     }
+//     else{
+//         activeImage.classList.remove("active-img"); 
+//         activeImage.parentNode.firstElementChild.classList.add("active-img")
+//     }
+    
+// }
+// rightIcon.addEventListener("click",function(){
+//     rightSlider();
+// })
+
+// leftIcon.addEventListener("click",function(){
+//     let activeImage=document.querySelector(".active-img");
+//     if(activeImage.previousElementSibling!=null){
+//         activeImage.classList.remove("active-img"); 
+//         activeImage.previousElementSibling.classList.add("active-img")
+//     }
+//     else{
+//         activeImage.classList.remove("active-img"); 
+//         activeImage.parentNode.lastElementChild.classList.add("active-img")
+//     }
+
+// })
+
+// setInterval(() => {
+//     rightSlider();
+// }, 2000);
+
+
+
